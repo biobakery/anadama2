e59e1a873a9e875260c34c869bc29cf299cc4e61
Randall Schwager <schwager@hsph.harvard.edu>
2014-06-30 20:52:27 +0000

    feature: generalize Main class for others to subclass

--- anadama/cli.py
+++ anadama/cli.py
@@ -3,6 +3,7 @@
 import sys
 
 from doit.doit_cmd import DoitMain
+from doit.cmd_base import DoitCmdBase
 
 from . import commands
 
@@ -10,12 +11,16 @@ class Main(DoitMain):
     
     def __init__(self, extra_cmds, *args, **kwargs):
         self._extra_cmds = extra_cmds
-        super(Main, self)__init__(*args, **kwargs)
+        super(Main, self).__init__(*args, **kwargs)
     
     def get_commands(self):
         cmds = DoitMain.get_commands(self)
         for extra_cmd_cls in self._extra_cmds:
-            extra_cmd = extra_cmd_cls()
+            if issubclass(extra_cmd_cls, DoitCmdBase):
+                extra_cmd = extra_cmd_cls(task_loader=self.task_loader)
+                extra_cmd.doit_app = self
+            else:
+                extra_cmd = extra_cmd_cls()
             cmds[extra_cmd.name] = extra_cmd
         return cmds
 
