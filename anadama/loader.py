fb1755b585b9710f95b396227ce2f362b5e47078
Randall Schwager <schwager@hsph.harvard.edu>
2015-01-13 13:37:25 -0500

    feature: add command to run arbitrary anadama.pipelines.Pipeline subclasses

---
+++ anadama/loader.py
@@ -0,0 +1,239 @@
+import re
+import importlib
+from collections import defaultdict
+
+from doit.cmd_base import TaskLoader
+from doit.exceptions import InvalidCommand
+
+from .util import filespec
+from .pipelines import Pipeline
+        
+opt_pipeline_argument = { 
+    "name"    : "pipeline_arg",
+    "long"    : "pipeline_arg",
+    "short"   : "f",
+    "type"    : list,
+    "default" : [],
+    "help"    : ("Arguments (typically, files; hence -f) "
+                 "to provide to the pipeline in 'key: value' format.\n"
+                 "Check the pipeline's documentation for appropriate options.\n"
+                 "Specify multiple options with multiple -f 'opt' flags.\n"
+                 "Values beginning with 'glob:' are expanded by shell glob.\n"
+                 "Values beginning with 're:' are expanded by regex.\n"
+                 "Non-glob and non-regex values with commas are interpreted "
+                 "as a list.")
+}
+
+opt_append_pipeline = {
+    "name"    : "append_pipeline",
+    "long"    : "append_pipeline",
+    "short"   : "A",
+    "type"    : list,
+    "default" : [],
+    "help"    : ("Append this pipeline to the main pipeline.\n"
+                 "Append multiple with many -A flags")
+}
+
+opt_pipeline_option = {
+    "name"    : "pipeline_option",
+    "long"    : "pipeline_option",
+    "short"   : "o",
+    "type"    : list,
+    "default" : [],
+    "help"    : ("Options to give to the workflows composing this pipeline.\n"
+                 "Specify options in 'workflowname.key: value' format "
+                 "e.g. sequence_convert.lenfilters_list: >=100.\n"
+                 "For boolean options, leave the value blank e.g. "
+                 "humann2.pick_frames: \n"
+                 "For lists, use commas e.g. "
+                 "sequence_convert.lenfilters_list: >=100,<300.\n"
+                 "For nested dictionaries, use more periods e.g. "
+                 "demultiplex.qiime_opts.M: 2")
+}
+                
+opt_skiptasks = {    
+    "name"    : "skip_tasks",
+    "long"    : "skip_tasks",
+    "short"   : "k",
+    "type"    : list,
+    "default" : [],
+    "help"    : ("defines criterion by which AnADAMA will filter out or skip when \n"
+                 "executing tasks in 'key: value' format. Add multiple filters with \n"
+                 "multiple -k flags. Children skipped tasks will also be skipped\n"
+                 "Example: 'name: humann' will skip any tasks that contain 'humann' \n"
+                 "in the task 'name' attribute and any tasks that depend on those \n"
+                 "tasks.")
+}
+
+opt_data_directory = {    
+    "name"    : "data_directory",
+    "long"    : "data_dir",
+    "short"   : "d",
+    "type"    : str,
+    "default" : './',
+    "help"    : "Base directory to search for files to feed into the pipeline."
+}
+
+opt_products_directory = {
+    "name"    : "products_directory",
+    "long"    : "products_dir",
+    "type"    : str,
+    "default" : './anadama_products',
+    "help"    : "Base directory to save data products."
+}
+
+class PipelineLoader(TaskLoader):
+
+    cmd_options = (opt_pipeline_argument, opt_data_directory, 
+                   opt_pipeline_option, opt_products_directory, 
+                   opt_append_pipeline, opt_skiptasks)
+
+    def __init__(self, *args, **kwargs):
+        self._pipeline_cls = None
+        super(PipelineLoader, self).__init__(*args, **kwargs)
+
+
+    @property
+    def pipeline_cls(self):
+        return self._pipeline_cls
+
+
+    @pipeline_cls.setter
+    def pipeline_cls(self, value):
+        if isinstance(value, Pipeline):
+            self._pipeline_cls = value
+        elif type(value) is str:
+            self._pipeline_cls = self._import(value)
+        else:
+            raise TypeError("Acceptable values are module "
+                            "strings or anadama.Pipeline")
+
+
+    def load_tasks(self, cmd, opt_values, pos_args):
+        args, kwargs = self._parse_args(self.pipeline_cls, opt_values, pos_args)
+        pipeline = self.pipeline_cls(*args, **kwargs)
+        for pipeline_name in opt_values['append_pipeline']:
+            cls = self._import(pipeline_name)
+            args, kwargs = self._parse_args(cls, opt_values, pos_args)
+            optional_pipeline = cls(*args, **kwargs)
+            pipeline.append(optional_pipeline)
+
+        config = pipeline.configure()
+        return pipeline.tasks(), config
+
+
+    def _parse_args(self, pipe_cls, opt_values, pos_args):
+        self.data_dir = opt_values['data_directory']
+
+        keyword_arguments = dict()
+        keyword_arguments['products_dir'] = opt_values['products_directory']
+        file_options = self._parse_file_arguments(opt_values, pipe_cls)
+        keyword_arguments.update(file_options)
+
+        wf_options = self._parse_workflow_options(opt_values, pipe_cls)
+        keyword_arguments['workflow_options'] = wf_options
+
+        skiptasks = self._parse_skiptasks(opt_values['skip_tasks'])
+        keyword_arguments['skipfilters'] = skiptasks
+
+        return [], keyword_arguments
+
+
+    def _parse_file_arguments(self, opt_values, pipe_cls):
+        for opt in opt_values['pipeline_arg']:
+            key, val = self._pipeline_option_split(opt)
+            if key in pipe_cls.products:
+                files = self._parse_file_pattern(val, self.data_dir)
+                yield key, files
+
+
+    def _parse_workflow_options(self, opt_values, pipe_cls):
+        ret = defaultdict(dict)
+        for workflow_opt_str in opt_values['pipeline_option']:
+            workflow_name, key_value = self._workflow_option_split(workflow_opt_str)
+            if workflow_name in pipe_cls.options:
+                ret[workflow_name].update(key_value)
+
+        return dict(ret)
+        
+
+    @staticmethod
+    def _parse_skiptasks(filter_strs):
+        ret = []
+        for s in filter_strs:
+            try:
+                key, regex = s.split(': ', 1)
+            except:
+                raise InvalidCommand("Unable to parse skip_task pattern."
+                                     "Be sure to split keys and values with ': '.")
+            try:
+                regex = re.compile(regex)
+            except Exception as e:
+                raise InvalidCommand("Unable to parse regular expression: "+e.message)
+
+            the_filter = lambda task_dict: regex.search(task_dict[key])
+            ret.append(the_filter)
+
+        return ret
+
+
+    @staticmethod
+    def _workflow_option_split(opt_str):
+        try:
+            name_key, value = opt_str.split(":", 1)
+            if ',' in value: 
+                value = value.split(",")
+        except ValueError:
+            value = None
+
+        packed = name_key.split('.')
+        if len(packed) < 2:
+            raise InvalidCommand(("Unable to determine workflow name in option %s. "
+                                  "Remember to prefix workflow options with the "
+                                  "workflow name e.g. humann2.pick_frames")%(opt_str))
+        elif len(packed) == 2:
+            name, key = packed
+            key_value = {key: value}
+        else:
+            name, to_be_nested = packed[0], packed[1:]
+            items = reversed(to_be_nested)
+            key_value = {items.next(): value}
+            for item in items:
+                key_value = {item: key_value}
+                
+        return name, key_value
+
+
+    @staticmethod
+    def _parse_file_pattern(pattern_str, data_dir):
+        try:
+            return filespec.parse(pattern_str, data_dir=data_dir)
+        except OSError as e:
+            raise InvalidCommand("Unable to expand %s: %s"%(pattern_str, e))
+
+
+    @staticmethod
+    def _pipeline_option_split(option):
+        try:
+            key, val = option.split(": ", 1)
+        except ValueError:
+            raise InvalidCommand(
+                ("Unable to parse pipeline option %s."
+                 "Keys must be separated with ': ' from values.")%(option)
+            )
+        return key, val
+
+
+    @staticmethod
+    def _import(pipeline_name):
+        try:
+            mod, pipeline_name = pipeline_name.split(":")
+            module = importlib.import_module(mod)
+            return getattr(module, pipeline_name)
+        except (ImportError, AttributeError) as e:
+            raise InvalidCommand(e.message)
+        except ValueError:
+            raise InvalidCommand(
+                "Unable to understand module name. "
+                "Try something like 'anadama_workflows.pipelines:WGSPipeline'"
+            )
---
+++ anadama/util/filespec.py
@@ -0,0 +1,26 @@
+import re
+import os
+import glob
+
+DEFAULT_DATA_DIR = "./"
+
+def parse(pattern_str, data_dir=DEFAULT_DATA_DIR):
+    if pattern_str.startswith("glob:"):
+        pattern = os.path.join(data_dir,
+                               pattern_str.split("glob:", 1)[1])
+        files = map(os.path.basename, glob.glob(pattern))
+        return files
+    elif pattern_str.startswith("re:"):
+        matcher = lambda s: re.search(pattern_str.split("re:", 1)[1], s)
+        files = filter(matcher, os.listdir(data_dir))
+        return files
+    elif ',' in pattern_str:
+        files = pattern_str.split(',')
+        nonexistent = [ not os.path.exists(f) for f in files ]
+        if nonexistent:
+            raise OSError("No such file or directory: "+", ".join(nonexistent))
+        return files
+    else:
+        if not os.path.exists(pattern_str):
+            raise OSError("No such file or directory: "+pattern_str)
+        return [pattern_str]
