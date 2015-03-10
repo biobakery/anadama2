633d8b8d24b1b2bf5f8398da7ee3457522a668ff
Randall Schwager <schwager@hsph.harvard.edu>
2015-03-10 19:31:17 +0000

    fix: catch filespec not matching any files

--- anadama/util/filespec.py
+++ anadama/util/filespec.py
@@ -9,18 +9,19 @@ def parse(pattern_str, data_dir=DEFAULT_DATA_DIR):
         pattern = os.path.join(data_dir,
                                pattern_str.split("glob:", 1)[1])
         files = map(os.path.abspath, glob.glob(pattern))
-        return files
     elif pattern_str.startswith("re:"):
         matcher = lambda s: re.search(pattern_str.split("re:", 1)[1], s)
         files = filter(matcher, os.walk(data_dir))
-        return files
     elif ',' in pattern_str:
         files = pattern_str.split(',')
         nonexistent = [ not os.path.exists(f) for f in files ]
         if nonexistent:
             raise OSError("No such file or directory: "+", ".join(nonexistent))
-        return files
     else:
         if not os.path.exists(pattern_str):
             raise OSError("No such file or directory: "+pattern_str)
-        return [pattern_str]
+        files = [pattern_str]
+
+    if not files:
+        raise ValueError("The pattern matched no files")
+    return files
