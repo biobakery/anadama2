90840c9edd23d546192803decbc948bf120a2038
Randall Schwager <schwager@hsph.harvard.edu>
2015-05-08 19:03:04 +0000

    feature: create error files and output files for executed tasks

--- anadama/reporter.py
+++ anadama/reporter.py
@@ -1,8 +1,22 @@
-
 from doit.reporter import ConsoleReporter
 from doit.reporter import REPORTERS as doit_REPORTERS
 from doit.action import CmdAction
 
+
+def _maybestrip(maybe_str):
+    if type(maybe_str) is str:
+        return maybe_str.strip()
+    else:
+        return maybe_str
+
+
+def _alltext(task):
+    return [
+        (_maybestrip(action.out), _maybestrip(action.err))
+        for action in task.actions
+    ]
+
+            
 class VerboseConsoleReporter(ConsoleReporter):
     def execute_task(self, task, *args, **kwargs):
         super(VerboseConsoleReporter, self).execute_task(task, *args, **kwargs)
@@ -12,6 +26,44 @@ class VerboseConsoleReporter(ConsoleReporter):
                     # I don't think calling expand_action has any side
                     # effects right now. hope it never does!
                     self.write(action.expand_action()+'\n')
+
+
+    def _find_output_files(self, task):
+        if not task.targets:
+            return None, None
+        fbase = task.targets[0]+"."
+        outs, errs = zip(*_alltext(task))
+        out_f, err_f = None, None
+        if any(outs):
+            out_f = open(fbase+"out", 'w')
+        if any(errs):
+            err_f = open(fbase+"err", 'w')
+        return (out_f, err_f)
+
+
+    def add_failure(self, task, exception):
+        if task.actions and task.name[0] != '_':
+            for action in task.actions:
+                if type(action.err) is str:
+                    action.err += ("\n" + str(exception))
+                else:
+                    action.err = str(exception)
+        return self.add_success(task)
+
+
+    def add_success(self, task):
+        out_f, err_f = self._find_output_files(task)
+        try:
+            for action in task.actions:
+                for f, attr in ((out_f, action.out), (err_f, action.err)):
+                    if attr and f:
+                        print >> f, attr
+        finally:
+            if out_f:
+                out_f.close()
+            if err_f:
+                err_f.close()
+
 
 REPORTERS = doit_REPORTERS
 REPORTERS['verbose'] = VerboseConsoleReporter
