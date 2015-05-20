7dc3633824603505ee840404d920f02ef15f4f6a
schwager-hsph <schwager@hsph.harvard.edu>
2015-05-20 09:17:13 -0400

    update copyright and new theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'anadama'
-copyright = u'2014, Randall Schwager and the Huttenhower Lab'
+copyright = u'2015, Randall Schwager and the Huttenhower Lab'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -104,7 +104,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
