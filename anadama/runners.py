2b85673c701c8bfdd9223cb4bafd39246f8fce8d
schwager-hsph <schwager@hsph.harvard.edu>
2016-03-02 11:58:59 -0500

    fill in local runner function

--- anadama/runners.py
+++ anadama/runners.py
@@ -1,5 +1,7 @@
+import traceback
 import cPickle as pickle
 from collections import namedtuple
+
 
 class TaskResult(namedtuple(
         "TaskResult", ["task_no", "errors", "dep_keys", "dep_compares"])):
@@ -8,33 +10,62 @@ class TaskResult(namedtuple(
     pass
 
 
+
+class TaskFailed(Exception):
+    def __init__(self, msg, task_no):
+        self.task_no = task_no
+        super(TaskFailed, self).__init__(msg)
+
+
+
 def default():
     pass
 
-def exception_result(exc, pkl):
-    return TaskResult(pickle.loads(pkl).task_no, str(exc), None, None)
 
-def run_task_locally(task_pkl):
+def exception_result(exc):
+    return TaskResult(exc.task_no, exc.msg, None, None)
+
+
+def _run_task_locally(task_pkl):
     task = pickle.loads(task_pkl)
+    for i, action_func in enumerate(task.actions):
+        try:
+            action_func()
+        except Exception as e:
+            msg = ("Error executing action {}. "
+                   "Original Exception: \n{}\n").format(i, e)
+            msg += "\n".join(traceback.format_stack())
+            raise TaskFailed(msg, task.task_no)
+
+    targ_keys, targ_compares = list(), list()
+    for target in task.targets:
+        targ_keys.append(target._key)
+        try:
+            targ_compares.append(list(target.compare()))
+        except Exception as e:
+            msg = "Failed to produce target `{}'. Original exception: {}"
+            raise TaskFailed(msg.format(target, e), task.task_no)
+            
+    return TaskResult(task.task_no, None, targ_keys, targ_compares)
 
 
-def LocalRunner(work_q, res_q):
+def run_local(work_q, res_q):
     while True:
         pkl = work_q.get()
         try:
-            result = run_task_locally(pkl)
+            result = _run_task_locally(pkl)
         except Exception as e:
             result = exception_result(e, pkl)
         res_q.put(result)
         work_q.task_done()
         
 
-def LSFRunner(workf_q, res_q):
+def run_lsf(workf_q, res_q):
     pass
 
-def SGERunner(GridRunner):
+def run_slurm(workf_q, res_q):
     pass
 
-def SLURMRunner(GridRunner):
+def run_sge(workf_q, res_q):
     pass
     
