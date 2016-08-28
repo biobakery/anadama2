16b3172925bcc463b06deb4b1bf33d1a5704f718
Randall Schwager <schwager@hsph.harvard.edu>
2016-08-27 22:12:27 -0400

    change do syntax in examples

--- examples/exe_check.py
+++ examples/exe_check.py
@@ -1,7 +1,7 @@
 from anadama2 import Workflow
 
 workflow = Workflow()
-workflow.do("ls /usr/bin/ | sort > @{global_exe.txt}")
-workflow.do("ls $HOME/.local/bin/ | sort > @{local_exe.txt}")
-workflow.do("join #{global_exe.txt} #{local_exe.txt} > @{match_exe.txt}")
+workflow.do("ls /usr/bin/ | sort > [t:global_exe.txt]")
+workflow.do("ls $HOME/.local/bin/ | sort > [t:local_exe.txt]")
+workflow.do("join [d:global_exe.txt] [d:local_exe.txt] > [t:match_exe.txt]")
 workflow.go()
