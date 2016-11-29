2c1598be811d217048b89018b188f10731fed9ba
Lauren McIver <lauren.j.mciver@gmail.com>
2016-11-29 17:41:14 -0500

    Add initial sbatch slurm run mode for command line actions, see ticket #29

--- anadama2/grid/slurm.py
+++ anadama2/grid/slurm.py
@@ -5,6 +5,13 @@ import sys
 import logging
 import itertools
 import threading
+import string
+import tempfile
+import subprocess
+import datetime
+import multiprocessing
+import time
+
 from math import exp
 from collections import namedtuple
 
@@ -90,10 +97,16 @@ class Slurm(Dummy):
     def __init__(self, partition, tmpdir=None, extra_srun_flags=[]):
         self.slurm_partition = partition
         self.slurm_tmpdir = tmpdir
+        if self.slurm_tmpdir:
+            # create the folder if it does not already exist
+            if not os.path.isdir(self.slurm_tmpdir):
+                os.makedirs(self.slurm_tmpdir)
+            
         self.extra_srun_flags = extra_srun_flags
         
         self.slurm_task_data = dict()
-
+        
+        self.slurm_queue = SLURMQueue()
 
     def _kwargs_extract(self, kwargs_dict):
         time = kwargs_dict.pop("time", None)
@@ -182,12 +195,100 @@ class Slurm(Dummy):
                           name="local", rate=jobs, default=True)
         runner.add_worker(SLURMWorker, name="slurm", rate=grid_jobs)
         runner.routes.update((
-            ( task_idx, ("slurm", extra) )
+            ( task_idx, ("slurm", list(extra)+[self.slurm_queue]) )
             for task_idx, extra in six.iteritems(self.slurm_task_data)
         ))
         return runner
 
+class SLURMQueue():
     
+    def __init__(self):
+        # this is the refresh rate for checking the queue, in seconds
+        self.refresh_rate = 300
+        # this is the last time the queue was checked
+        self.last_check = time.time()
+        self.sacct = None
+        # create a lock for jobs in queue
+        self.lock_status = multiprocessing.Lock()
+        self.lock_submit = multiprocessing.Lock()
+        
+    def get_slurm_status(self, force=None):
+        """ Get the queue accounting stats """
+        
+        # lock to prevent race conditions with status update
+        self.lock_status.acquire()
+        
+        # check the last time the queue was captured
+        current_time = time.time()
+        if current_time - self.last_check > self.refresh_rate or force or self.sacct is None:
+            self.last_check = current_time
+            self.sacct = self._run_slurm_sacct()
+            
+        self.lock_status.release()
+        
+        return self.sacct
+            
+    def _run_slurm_sacct(self):
+        """ Check the status of the slurm ids """
+        stdout=subprocess.check_output(["sacct","-o","JobID,State,AllocCPUs,Elapsed,MaxRSS"])
+
+        # remove the header information from the status lines
+        # split each line and remove empty lines
+        info=filter(lambda x: x, [line.rstrip().split() for line in stdout.split("\n")[2:]])
+
+        return list(info)
+    
+    def _get_all_stats_for_jobid(self,jobid,wait=None):
+        """ Get all the stats for a specific job id """
+        
+        # use the existing stats, to get the information for the jobid
+        job_stats=filter(lambda x: x[0].startswith(jobid),self.get_slurm_status())
+        
+        # if the job stats are not found for the job, wait for the stats to refresh
+        if wait or not job_stats:
+            wait_time=self.refresh_rate - (time.time() - self.last_check) + 10
+            time.sleep(wait_time)
+            # refresh the stats
+            job_stats=filter(lambda x: x[0].startswith(jobid),self.get_slurm_status())
+        
+        return list(job_stats)
+    
+    def get_status(self, jobid):
+        """ Check the status of the job """
+        
+        info=self._get_all_stats_for_jobid(jobid)
+        
+        return info[0][1]
+
+    def get_benchmark(self,jobid):
+        """ Check the benchmarking stats of the slurm id """
+
+        # wait for the next refresh of the stats so running jobs
+        # are shown as completed with memory stats
+        info=self._get_all_stats_for_jobid(jobid,wait=True)
+        # get the memory max from the batch line which is the second line of output
+        elapsed=info[0][3]
+        cpus=info[0][2]
+        memory="{:.1f}".format(int(info[1][4].replace("K",""))/1024.0)
+
+        return elapsed, cpus, memory    
+
+    def submit_job(self,slurm_script):
+        """ Submit the slurm jobs and return the slurm job id """
+        
+        # lock so only one task submits jobs to the queue at a time
+        self.lock_submit.acquire()
+
+        # submit the job and get the slurm id
+        stdout=subprocess.check_output(["sbatch",slurm_script])
+        slurm_jobid=stdout.rstrip().split()[-1]
+        
+        # pause for the scheduler
+        time.sleep(3)
+        
+        self.lock_submit.release()
+    
+        return slurm_jobid
 
 class SLURMWorker(threading.Thread):
 
@@ -204,11 +305,123 @@ class SLURMWorker(threading.Thread):
     def run(self):
         return runners.worker_run_loop(self.work_q, self.result_q, 
                                        _run_task_slurm)
-
-
-
+        
 def _run_task_slurm(task, extra):
-    (perf, partition, tmpdir, extra_srun_flags) = extra
+    # if the task is a function, then use pickle srun interface
+    if six.callable(task.actions[0]):
+        return _run_task_function_slurm(task, extra)
+    else:
+        return _run_task_command_slurm(task, extra)
+        
+def _create_slurm_script(partition,cpus,minutes,memory,command,taskid,dir):
+    """ Create a slurm script from the template also creating temp stdout and stderr files """
+    
+    slurm_template = string.Template("\n".join(["#!/bin/bash ",
+        "#SBATCH -p ${partition}",
+        "#SBATCH -N 1 ",
+        "#SBATCH -n ${cpus}",
+        "#SBATCH -t ${time}",
+        "#SBATCH --mem ${memory}",
+        "#SBATCH -o ${output}",
+        "#SBATCH -e ${error}",
+        "",
+        "${command}",
+        "${rc_command}"]))   
+
+    # create temp files for stdout, stderr, and return code    
+    handle_out, out_file=tempfile.mkstemp(suffix=".out",prefix="task_"+str(taskid)+"_",dir=dir)
+    os.close(handle_out)
+    handle_err, error_file=tempfile.mkstemp(suffix=".err",prefix="task_"+str(taskid)+"_",dir=dir)
+    os.close(handle_err)
+    handle_rc, rc_file=tempfile.mkstemp(suffix=".rc",prefix="task_"+str(taskid)+"_",dir=dir)
+    os.close(handle_rc)
+
+    # convert the minutes to the time string
+    time=str(datetime.timedelta(minutes=minutes))
+
+    slurm=slurm_template.substitute(partition=partition,cpus=cpus,time=time,
+        memory=memory,command=command,output=out_file,error=error_file,rc_command="echo $? > "+rc_file)
+    file_handle, slurm_file=tempfile.mkstemp(suffix=".slurm",prefix="task_"+str(taskid)+"_",dir=dir)
+    os.write(file_handle,slurm)
+    os.close(file_handle)
+    
+    return slurm_file, out_file, error_file, rc_file
+
+def _log_slurm_output(taskid, file, file_type):
+    """ Write the slurm stdout/stderr files to the log """
+    
+    try:
+        lines=open(file).readlines()
+    except EnvironmentError:
+        lines=[]
+        
+    logging.info("Slurm %s from task id %s:\n%s",taskid, file_type, "\n".join(lines))
+
+def _run_task_command_slurm(task, extra):
+    (perf, partition, tmpdir, extra_srun_flags, slurm_queue) = extra
+    # create a slurm script and stdout/stderr files for this task
+    commands="\n".join(task.actions)
+    logging.info("Running commands for task id %s:\n%s", task.task_no, commands)
+    
+    slurm_script, out_file, error_file, rc_file = _create_slurm_script(partition,
+        perf.cores, perf.time, perf.mem, commands, task.task_no, tmpdir)
+
+    logging.info("Created slurm files for task id %s: %s, %s, %s, %s",
+        task.task_no, slurm_script, out_file, error_file, rc_file)
+
+    # submit the job
+    slurm_jobid = slurm_queue.submit_job(slurm_script)
+    
+    logging.info("Submitted job for task id %s: slurm id %s", task.task_no,
+        slurm_jobid)
+    
+    # poll to check for status
+    for tries in itertools.count(1):
+        # only check status at intervals
+        time.sleep(10)
+        
+        # check if a slurm error is written to the output file
+        try:
+            slurm_errors=subprocess.check_output(["grep","-F","slurmstepd: error:",error_file]).split("\n")
+        except (EnvironmentError, subprocess.CalledProcessError):
+            slurm_errors=[]
+            
+        cancelled=list(filter(lambda x: "CANCELLED" in x, slurm_errors))
+        if cancelled:
+            # check for time or memory
+            if "TIME LIMIT" in cancelled[0]:
+                logging.info("Slurm task %s cancelled due to time limit", slurm_jobid)
+            else:
+                logging.info("Slurm task %s cancelled due to memory limit", slurm_jobid)
+            break
+
+        # check the queue stats
+        slurm_job_status = slurm_queue.get_status(slurm_jobid)
+        
+        logging.info("Status for job id %s with slurm id %s is %s",task.task_no,
+            slurm_jobid,slurm_job_status)
+        
+        if slurm_job_status in ["COMPLETED","FAILED","TIMEOUT"]:
+            break
+        
+        # check if the return code file is written
+        if os.path.getsize(rc_file) > 0:
+            break
+        
+    # get the benchmarking data
+    elapsed, cpus, memory = slurm_queue.get_benchmark(slurm_jobid)
+    logging.info("Benchmark information for job id %s:\nElapsed: %s minutes\nCPUs: %s\nMEMORY: %s MB",
+        task.task_no, elapsed,cpus,memory)
+    
+    # write the stdout and stderr to the log
+    _log_slurm_output(task.task_no, out_file, "standard output")
+    _log_slurm_output(task.task_no, error_file, "standard error")
+    _log_slurm_output(task.task_no, rc_file, "return code")
+            
+    return runners._get_task_result(task)
+
+def _run_task_function_slurm(task, extra):
+    (perf, partition, tmpdir, extra_srun_flags, slurm_queue) = extra
     script_path = picklerunner.tmp(task, dir=tmpdir).path
     job_name = "task{}:{}".format(task.task_no, underscore(task.name))
     mem, time = perf.mem, perf.time
@@ -248,5 +461,4 @@ def _run_task_slurm(task, extra):
     elif extra_error: # (result is not None) is implicit here
         result = result._replace(error=str(result.error)+extra_error)
     return result
-        
 
