ee7087ad2356d0383b98bec3954bd296b2ef30f2
Lauren McIver <lauren.j.mciver@gmail.com>
2017-03-16 13:50:00 -0600

    Add simple workflow example that downloads three files

---
+++ examples/download_files.py
@@ -0,0 +1,17 @@
+
+from anadama2 import Workflow
+
+workflow = Workflow(remove_options=["input","output"])
+
+downloads=["ftp://public-ftp.hmpdacc.org/HM16STR/by_sample/SRS011275.fsa.gz",
+    "ftp://public-ftp.hmpdacc.org/HM16STR/by_sample/SRS011273.fsa.gz",
+    "ftp://public-ftp.hmpdacc.org/HM16STR/by_sample/SRS011180.fsa.gz"]
+
+for link in downloads:
+    workflow.add_task(
+        "wget [args[0]]",
+        targets=link.split("/")[-1],
+        args=link)
+
+workflow.go()
+
