a9d948ad73aa4b7eeee60332b07976b54879e316
Lauren McIver <lauren.j.mciver@gmail.com>
2017-03-17 19:18:50 -0600

    Set the file name in the download example for multiple runs that overwrite

--- examples/download_files.py
+++ examples/download_files.py
@@ -9,7 +9,7 @@ downloads=["ftp://public-ftp.hmpdacc.org/HM16STR/by_sample/SRS011275.fsa.gz",
 
 for link in downloads:
     workflow.add_task(
-        "wget [args[0]]",
+        "wget -O [targets[0]] [args[0]]",
         targets=link.split("/")[-1],
         args=link)
 
