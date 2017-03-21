2af7db8cc4a01ea9d6256f7fe337f0dffa9d28f2
Lauren McIver <lauren.j.mciver@gmail.com>
2017-03-21 14:32:32 -0600

    Update container example

--- examples/kvcontainer.py
+++ examples/kvcontainer.py
@@ -1,14 +1,24 @@
-# -*- coding: utf-8 -*-
 import anadama2.tracked
 from anadama2 import Workflow
 
-ctx = Workflow(remove_options=["input","output"])
-step1_const = anadama2.tracked.Container(a = 20)
-step1 = ctx.add_task("echo [depends[0]] > [targets[0]]",
-                     depends=step1_const.a, targets="step1.txt",
-                     name="Step 1")
+workflow = Workflow(remove_options=["input","output"])
 
-step2 = ctx.add_task("p=$(cat [depends[0]]); echo $p > [targets[0]]",
-                     depends=step1.targets[0], targets="step2.txt",
-                     name="Step 2")
-ctx.go()
+# create a container class to track
+container = anadama2.tracked.Container(a = 20)
+
+# add a task that depends on the "a" variable in the container
+task1=workflow.add_task(
+    "echo [depends[0]] > [targets[0]]",
+    depends=container.a, 
+    targets="echo.txt",
+    name="task1")
+
+# add a task that depends on the targets of task1 
+task2=workflow.add_task(
+    "p=$(cat [depends[0]]); echo $p > [targets[0]]",
+    depends=task1.targets[0],
+    targets="echo2.txt",
+    name="task2")
+
+workflow.go()
+
