d2ea1a187f25bb99a5d90c646c695b141a011b65
ljmciver <lauren.j.mciver@gmail.com>
2019-01-10 16:52:37 +0000

    For multiple aws queues, change the update to add to get status from both for stdout progress

--- anadama2/grid/aws.py
+++ anadama2/grid/aws.py
@@ -267,10 +267,12 @@ class AWSQueue(GridQueue):
             if self.partition_long != self.partition_short:
                 if nextToken:
                     response = self.client.list_jobs(jobQueue=self.partition_short,jobStatus=state,nextToken=nextToken)
-                    response.update(self.client.list_jobs(jobQueue=self.partition_long,jobStatus=state,nextToken=nextToken))
+                    response2 = self.client.list_jobs(jobQueue=self.partition_long,jobStatus=state,nextToken=nextToken)
+                    response['jobSummaryList'].append(response2['jobSummaryList'])
                 else:
                     response = self.client.list_jobs(jobQueue=self.partition_short,jobStatus=state)
-                    response.update(self.client.list_jobs(jobQueue=self.partition_long,jobStatus=state))
+                    response2 = self.client.list_jobs(jobQueue=self.partition_long,jobStatus=state)
+                    response['jobSummaryList'].append(response2['jobSummaryList'])
             else:
                 if nextToken:
                     response = self.client.list_jobs(jobQueue=self.partition,jobStatus=state,nextToken=nextToken)
