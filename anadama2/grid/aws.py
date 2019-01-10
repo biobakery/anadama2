87682394163a29b56c0d01fead2ec707b21cf2d6
ljmciver <lauren.j.mciver@gmail.com>
2019-01-10 20:07:03 +0000

    Allow for empty job lists with multiple aws queues

--- anadama2/grid/aws.py
+++ anadama2/grid/aws.py
@@ -268,11 +268,13 @@ class AWSQueue(GridQueue):
                 if nextToken:
                     response = self.client.list_jobs(jobQueue=self.partition_short,jobStatus=state,nextToken=nextToken)
                     response2 = self.client.list_jobs(jobQueue=self.partition_long,jobStatus=state,nextToken=nextToken)
-                    response['jobSummaryList'].append(response2['jobSummaryList'])
+                    if response2['jobSummaryList']:
+                        response['jobSummaryList'].append(response2['jobSummaryList'])
                 else:
                     response = self.client.list_jobs(jobQueue=self.partition_short,jobStatus=state)
                     response2 = self.client.list_jobs(jobQueue=self.partition_long,jobStatus=state)
-                    response['jobSummaryList'].append(response2['jobSummaryList'])
+                    if response2['jobSummaryList']:
+                        response['jobSummaryList'].append(response2['jobSummaryList'])
             else:
                 if nextToken:
                     response = self.client.list_jobs(jobQueue=self.partition,jobStatus=state,nextToken=nextToken)
